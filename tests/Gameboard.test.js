7f0dd510aa95ad9095edd59e00ff810d0aa82e65
benlgw <ben.greaveswarwick@tutamail.com>
2026-07-04 16:22:35 +0100

    test: verify board can place ship

---
+++ tests/Gameboard.test.js
@@ -0,0 +1,13 @@
+import { test, expect } from "vitest";
+import Gameboard from "./../src/modules/Gameboard.js";
+import Ship from "./../src/modules/Ship.js";
+
+const gameboard = new Gameboard();
+
+test("board can place a ship", () => {
+    const x = 1;
+    const y = 1;
+    const ship = new Ship();
+    gameboard.placeShip(x, y, ship);
+    expect(gameboard.grid[x - 1][y - 1]).toBe(ship);
+});

b279e148034cdbbcc4d2fcfc283f933983ad8d56
benlgw <ben.greaveswarwick@tutamail.com>
2026-07-04 17:10:03 +0100

    fix: fix x representing y and y representing x

--- tests/Gameboard.test.js
+++ tests/Gameboard.test.js
@@ -5,19 +5,19 @@ import Ship from "./../src/modules/Ship.js";
 const gameboard = new Gameboard();
 
 test("board can place a ship", () => {
-    const x = 1;
     const y = 1;
+    const x = 1;
     const ship = new Ship();
-    gameboard.placeShip(x, y, ship);
-    expect(gameboard.grid[x - 1][y - 1]).toBe(ship);
+    gameboard.placeShip(y, x, ship);
+    expect(gameboard.grid[y - 1][x - 1]).toBe(ship);
 });
 
 test("board places ship according to length", () => {
-    const x = 1;
     const y = 1;
+    const x = 1;
     const ship = new Ship();
-    gameboard.placeShip(x, y, ship);
-    expect(gameboard.grid[x - 1][y - 1]).toBe(ship);
-    expect(gameboard.grid[x][y - 1]).toBe(ship);
-    expect(gameboard.grid[x + 1][y - 1]).toBe(ship);
+    gameboard.placeShip(y, x, ship);
+    expect(gameboard.grid[y - 1][x - 1]).toBe(ship);
+    expect(gameboard.grid[y][y - 1]).toBe(ship);
+    expect(gameboard.grid[y + 1][x - 1]).toBe(ship);
 });

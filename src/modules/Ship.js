d5d84739d9d4364a5c7e02895cb1a06b97e3787a
benlgw <ben.greaveswarwick@tutamail.com>
2026-07-04 15:19:10 +0100

    feat: add attributes to Ship class

---
+++ src/modules/Ship.js
@@ -0,0 +1,7 @@
+export default class Ship {
+    constructor() {
+        this.name = "ship"; // To be updated to official battleship names on ship creation
+        this.hits = 0;
+        this.sunk = false;
+    }
+}

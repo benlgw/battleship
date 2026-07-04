ffa363f9ace8c37d16fb4b2d2278291c5d338b75
benlgw <ben.greaveswarwick@tutamail.com>
2026-07-04 15:43:04 +0100

    feat: add ability for ship to take hits

--- src/modules/Ship.js
+++ src/modules/Ship.js
@@ -1,7 +1,12 @@
 export default class Ship {
     constructor() {
         this.name = "ship"; // To be updated to official battleship names on ship creation
+        this.length = 3;
         this.hits = 0;
         this.sunk = false;
+    }
+
+    hit() {
+        this.hits++;
     }
 }

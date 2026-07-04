64d4fbb600e81169ebade5eecdf9388cc77fc573
benlgw <ben.greaveswarwick@tutamail.com>
2026-07-04 16:32:12 +0100

    test: verify board places ship according to length

--- tests/Gameboard.test.js
+++ tests/Gameboard.test.js
@@ -11,3 +11,13 @@ test("board can place a ship", () => {
     gameboard.placeShip(x, y, ship);
     expect(gameboard.grid[x - 1][y - 1]).toBe(ship);
 });
+
+test("board places ship according to length", () => {
+    const x = 1;
+    const y = 1;
+    const ship = new Ship();
+    gameboard.placeShip(x, y, ship);
+    expect(gameboard.grid[x - 1][y - 1]).toBe(ship);
+    expect(gameboard.grid[x][y - 1]).toBe(ship);
+    expect(gameboard.grid[x + 1][y - 1]).toBe(ship);
+});

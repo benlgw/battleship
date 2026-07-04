74d7ffcd9a39ea3c3a7911cadc63749efcda421a
benlgw <ben.greaveswarwick@tutamail.com>
2026-07-04 17:26:19 +0100

    test: verify board does not place ship if out of bounds

--- tests/Gameboard.test.js
+++ tests/Gameboard.test.js
@@ -21,3 +21,12 @@ test("board places ship according to length", () => {
     expect(gameboard.grid[y - 1][x]).toBe(ship);
     expect(gameboard.grid[y - 1][x + 1]).toBe(ship);
 });
+
+test("board does not place ship if out of bounds", () => {
+    const gameboard = new Gameboard();
+    const y = 1;
+    const x = 9; // for ship length of 3 it can only place the first 2 pieces of the ship
+    const ship = new Ship();
+    expect(gameboard.placeShip(y, x, ship)).toBe(false);
+    expect(Object.hasOwn(gameboard.grid[y - 1], ship)).toBe(false);
+});

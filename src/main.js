3adc92bdcc6fa8e91f06bcb84ed605b8500feeb1
benlgw <ben.greaveswarwick@tutamail.com>
2026-07-05 14:51:28 +0100

    add event listener for enemy cells

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import "./reset.css";
 import "./style.css";
 
 import Player from "./modules/Player.js";
-import Ship from "./modules/Ship.js";
 import DisplayController from "./modules/DisplayController.js";
 
 const dom = new DisplayController();
@@ -10,20 +9,31 @@ const dom = new DisplayController();
 const player = new Player();
 const computer = new Player();
 
+computer.randomizeShips();
+
 document.addEventListener("DOMContentLoaded", () => {
     dom.createBoards();
+
     const randomizeButtons = document.querySelectorAll("button");
     randomizeButtons.forEach((button) => {
         button.addEventListener("click", () => {
             if (button.textContent.split(" ") === "Player") {
                 player.randomizeShips();
+                player.showShips();
             }
             if (button.textContent.split(" ") === "Player") {
                 computer.randomizeShips();
+
+                const computerBoard = document.querySelector("#computer");
+                const computerCells = computerBoard.querySelectorAll(".cell");
+                computerCells.forEach((cell) => {
+                    cell.addEventListener("click", (event) => {
+                        const y = event.target;
+                        console.log(y);
+                        computer.attack();
+                    });
+                });
             }
         });
     });
 });
-
-// Place computer ships in random position
-computer.randomizeShips();

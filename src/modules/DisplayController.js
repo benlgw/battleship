ba46db7d6669644d635935e9fd8d885ca8a410b7
benlgw <ben.greaveswarwick@tutamail.com>
2026-07-05 14:41:39 +0100

    add y and x co-ordinates to cells

--- src/modules/DisplayController.js
+++ src/modules/DisplayController.js
@@ -3,15 +3,23 @@ const computerBoard = document.querySelector("#computer");
 
 export default class DisplayController {
     createBoards() {
-        for (let i = 1; i <= 100; i++) {
-            const cell = document.createElement("div");
-            cell.classList.add("cell");
-            playerBoard.append(cell);
+        for (let i = 1; i <= 10; i++) {
+            for (let j = 1; j <= 10; j++) {
+                const cell = document.createElement("div");
+                cell.classList.add("cell");
+                cell.setAttribute("x", i);
+                cell.setAttribute("y", j);
+                playerBoard.append(cell);
+            }
         }
-        for (let i = 1; i <= 100; i++) {
-            const cell = document.createElement("div");
-            cell.classList.add("cell");
-            computerBoard.append(cell);
+        for (let i = 1; i <= 10; i++) {
+            for (let j = 1; j <= 10; j++) {
+                const cell = document.createElement("div");
+                cell.classList.add("cell");
+                cell.setAttribute("x", i);
+                cell.setAttribute("y", j);
+                computerBoard.append(cell);
+            }
         }
     }
 }

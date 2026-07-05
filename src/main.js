f7aab7a7fbede95b0807745305dddeebb64085c2
benlgw <ben.greaveswarwick@tutamail.com>
2026-07-05 13:34:12 +0100

    create the player and computer grids

--- src/main.js
+++ src/main.js
@@ -1,1 +1,14 @@
+import "./reset.css";
 import "./style.css";
+
+import Player from "./modules/Player.js";
+import DisplayController from "./modules/DisplayController.js";
+
+const dom = new DisplayController();
+
+document.addEventListener("DOMContentLoaded", () => {
+    dom.createBoards();
+});
+
+const player = new Player();
+const computer = new Player();

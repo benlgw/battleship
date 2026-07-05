f2ce4611c4a4ae8bd296a2589e9f24e54d545248
benlgw <ben.greaveswarwick@tutamail.com>
2026-07-05 13:42:14 +0100

    add ability to place ships in random positions

--- src/modules/Player.js
+++ src/modules/Player.js
@@ -8,4 +8,12 @@ export default class Player {
     placeShip(y, x, ship) {
         this.gameboard.placeShip(y, x, ship);
     }
+
+    randomizeShips() {
+        for (let i = 1; i <= 3; i++) {
+            const randomY = Math.floor(Math.random * 7);
+            const randomX = Math.floor(Math.random * 7);
+            computer.placeShip(randomY, randomX, new Ship());
+        }
+    }
 }
